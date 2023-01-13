b9ade1b9ad6d8d87738edfaa68c8004ef7bf76e2
Jayy <jordan@jayy.online>
2023-01-13 13:47:24 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use winit::event_loop::EventLoop;
+use winit::window::WindowBuilder;
+
 fn main () {
-    println!("hello");
+    let event_loop = EventLoop::new();
+
+    let builder = WindowBuilder::new();
+
+    builder.build(&event_loop).expect("Window did not build");
+
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_poll();
+    })
 }
