19eb350f695a78ead6416095022583b1ff965d76
Jayy <jordan@jayy.online>
2023-01-22 03:34:34 +0000

    Get window working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,44 @@
+use WindowEvent::CloseRequested;
+use winit::dpi::LogicalSize;
+use winit::event::{DeviceId, Event, WindowEvent};
+use winit::event::WindowEvent::CursorMoved;
 use winit::event_loop::EventLoop;
 use winit::window::WindowBuilder;
 
 fn main () {
     let event_loop = EventLoop::new();
 
-    let builder = WindowBuilder::new();
+    let builder = WindowBuilder::new()
+        .with_inner_size(LogicalSize::new(512, 512))
+        .with_title("OpenGL sandbox");
 
-    builder.build(&event_loop).expect("Window did not build");
+    // builder.build(&event_loop).expect("Window did not build");
+    let window = builder.build(&event_loop).unwrap();
+
+    let size = window.primary_monitor().unwrap().size();
 
     event_loop.run(move |event, _, control_flow| {
-        control_flow.set_poll();
+        control_flow.set_wait();
+
+        match event {
+            Event::WindowEvent {
+                event: CloseRequested,
+                ..
+            } => {
+                println!("The close button was pressed; stopping");
+                control_flow.set_exit();
+            },
+            Event::WindowEvent {
+                event: CursorMoved { position, .. },
+                ..
+            } => {
+                println!("Cursor position x: {}  y: {}", position.x, position.y);
+            },
+            Event::RedrawRequested(_) => {
+                println!("Redraw Requested!");
+                println!("width: {} height: {}", size.width, size.height);
+            },
+            _ => ()
+        }
     })
 }
